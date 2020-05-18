4d75c8b40e032fea601eb448d4dbdd29d7458a52
Shubham Singh <shubham88singh@gmail.com>
2020-05-18 07:57:24 +0530

    Setting the tailwind css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-6xl text-blue-400">Shubham Singh</h1>
+      
+    </div>
+  );
+}
+
+export default App;
